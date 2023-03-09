b875f3b5acef28b4dc4cd068fb4b3d796359ac69
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-09 17:46:16 +0100

    done

--- slot_machine.py
+++ slot_machine.py
@@ -7,44 +7,69 @@ MIN_BET = 1
 ROWS = 3
 COLS = 3
 
-symbols = {
+symbols_quantity = {
     "A": 2,
     "B": 4,
     "C": 6,
     "D": 8
 }
 
-#generate items that will be in the slot machine
+symbols_multiplier = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
 
-def gte_slot_machine_spin(rows, cols, symbols):
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    winning_lines = []
+    for line in range(lines):
+        symbol = columns[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            winning_lines.append(line + 1)
+        
+    return winnings, winning_lines
+
+##################
+
+def gen_slot_machine_items(rows, cols, symbols_quantity):
     all_symbols = []
-    for symbol, symbol_count in symbols.items():
+    for symbol, symbol_count in symbols_quantity.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
     columns = []  #rows
     for _ in range(cols):                          
         column = []
-        current_symbols = all_symbols[:]            #copy not reference of all symboles
+        current_symbols = all_symbols[:]          
         for _ in range(rows):                    
-            value = random.choice(current_symbols)  #picks random value from list
-            current_symbols.remove(value)           #don't pick it again
+            value = random.choice(current_symbols) 
+            current_symbols.remove(value)         
             column.append(value)                   
 
         columns.append(column) 
 
     return columns
 
-def print_slot_machine(columns):  #flipping aka transposing
+##################
+
+def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else:
-                print(columns[row])
+                print(column[row], end="")
 
+        print()
 
-
+##################
 
 def deposit():
     while True:
@@ -60,6 +85,8 @@ def deposit():
 
     return amount
 
+##################
+
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
@@ -73,6 +100,8 @@ def get_number_of_lines():
             print("Please enter a number.")
 
     return lines
+
+##################
 
 def get_bet():
     while True:
@@ -88,10 +117,9 @@ def get_bet():
 
     return amount
 
+##################
 
-
-def main():
-    balance = deposit()
+def spin(balance):
     lines = get_number_of_lines()
     while True:
         bet = get_bet()
@@ -103,6 +131,26 @@ def main():
             break
         
     print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+
+    slots = gen_slot_machine_items(ROWS, COLS, symbols_quantity)
+    print_slot_machine(slots)
+    winnings, winning_lines = check_winnings(slots, lines, bet, symbols_multiplier)
+    print(f"You won ${winnings}.")
+    print(f"You won on lines:", *winning_lines)
+    return winnings - total_bet
+
+##################
+
+def main():
+    balance = deposit()
+    while True:
+        print(f"Current balance is ${balance}")
+        answer = input("Press enter to play (q to quit).")
+        if answer.lower() == "q":
+            break
+        balance += spin(balance)
+
+    print(f"You left with ${balance}")
 
 
 main()
